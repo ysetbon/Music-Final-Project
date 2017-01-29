e3526027a27334cba763857365a533fa96242d47
David Badalov <davidbadalov1@gmail.com>
2017-01-29 22:08:02 +0200

    - Base references added

--- Music Final Project/Music Final Project/Form1.cs
+++ Music Final Project/Music Final Project/Form1.cs
@@ -7,14 +7,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using NFugue.Playing;
 
 namespace Music_Final_Project
 {
     public partial class Form1 : Form
     {
+        Player m_Player;
+
         public Form1()
         {
+            m_Player = new Player();
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            m_Player.Play("CEG");
         }
     }
 }
